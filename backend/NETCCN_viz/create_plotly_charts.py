b245fc6e32a1ac4366ef7af2a5a3bf1cf4f367d2
mariemloukill <mariemlouki1@gmail.com>
2022-04-19 01:01:40 +0100

    added waterfall and edited demographics format

--- backend/NETCCN_viz/create_plotly_charts.py
+++ backend/NETCCN_viz/create_plotly_charts.py
@@ -26,6 +26,8 @@ NETCCN= pd.read_excel("/mnt/scratch/datasources/netccn/NETCCN_Central_active_com
 print("loading cdc SVI..")
 svi=pd.read_csv("/mnt/scratch/datasources/cdc-svi/SVI2018_US_COUNTY.csv",dtype={'FIPS':'object'}) #link to data https://www.atsdr.cdc.gov/placeandhealth/svi/data_documentation_download.html
 
+print ("loading hhs utilization")
+utilizations= pd.read_csv("/mnt/scratch/datasources/healthdata.gov/utilizations.csv")
 
 #---------------Data cleaning and preparing---------------#
 NETCCN["StatusLogStatusReceived_Start"]=pd.to_datetime(NETCCN["StatusLogStatusReceived_Start"],format='%Y/%m/%d').dt.date
@@ -66,6 +68,9 @@ us_age17= round((sum(demographics["E_AGE17"])/us_population)*100,1)
 us_disability= round((sum(demographics["E_DISABL"])/us_population)*100,1)
 us_uninsured= round((sum(demographics["E_UNINSUR"])/us_population)*100,1)
 
+utilizations = utilizations.sort_values(by="collection_week")
+utilizations=utilizations.replace(-999999, 0) #replacing missing value 
+utilizations['collection_week'] = pd.to_datetime(utilizations["collection_week"],format='%Y/%m/%d').dt.date
 
 def covid_transmission_charts():
   print("creating covid_transmission_charts-----------------------")
@@ -213,38 +218,139 @@ def demographics_table():
         print("creating demographics file for " + fipscode)
         county= demographics[demographics["FIPS"]==fipscode]
         county_population= county.iloc[0]["E_TOTPOP"]
-        county_poverty=round((county.iloc[0]["E_POV"]/county_population)*100,1)
-        county_unemp= round((county.iloc[0]["E_UNEMP"]/county_population)*100,1)
-        county_age65= round((county.iloc[0]["E_AGE65"]/county_population)*100,1)
-        county_age17= round((county.iloc[0]["E_AGE17"]/county_population)*100,1)
-        county_disability=round((county.iloc[0]["E_DISABL"]/county_population)*100,1)
-        county_uninsured= round((county.iloc[0]["E_UNINSUR"]/county_population)*100,1)
+        county_poverty=round((county.iloc[0]["E_POV"]/county_population),3)
+        county_unemp= round((county.iloc[0]["E_UNEMP"]/county_population),3)
+        county_age65= round((county.iloc[0]["E_AGE65"]/county_population),3)
+        county_age17= round((county.iloc[0]["E_AGE17"]/county_population),3)
+        county_disability=round((county.iloc[0]["E_DISABL"]/county_population),3)
+        county_uninsured= round((county.iloc[0]["E_UNINSUR"]/county_population),3)
     
     #calculating metrics for state
         state=demographics[demographics["STATE"]==county.iloc[0]["STATE"]]
         state_population= sum(state["E_TOTPOP"])
-        state_poverty= round((sum(state["E_POV"])/state_population)*100,1)
-        state_unemp= round((sum(state["E_UNEMP"])/state_population)*100,1)
-        state_age65= round((sum(state["E_AGE65"])/state_population)*100,1)
-        state_age17= round((sum(state["E_AGE17"])/state_population)*100,1)
-        state_disability= round((sum(state["E_DISABL"])/state_population)*100,1)
-        state_uninsured= round((sum(state["E_UNINSUR"])/state_population)*100,1)
+        state_poverty= round((sum(state["E_POV"])/state_population),3)
+        state_unemp= round((sum(state["E_UNEMP"])/state_population),3)
+        state_age65= round((sum(state["E_AGE65"])/state_population),3)
+        state_age17= round((sum(state["E_AGE17"])/state_population),3)
+        state_disability= round((sum(state["E_DISABL"])/state_population),3)
+        state_uninsured= round((sum(state["E_UNINSUR"])/state_population),3)
 
-        cell_text = [["", county.iloc[0]["COUNTY"]+ " County", 'State: '+county.iloc[0]["STATE"].lower().title(), 'U.S.'],
-        ["Population ", "{:,}".format(county_population), "{:,}".format(state_population), "{:,}".format(us_population)],
-        [ "Percentage of Population below poverty",str(county_poverty)+'%', str(state_poverty)+'%', str(us_poverty)+'%'],
-        [ "Percentage of Unemployed Population",str(county_unemp)+'%', str(state_unemp)+'%', str(us_unemp)+'%'],
-        [  "Percentage of Uninsured Population ",str(county_uninsured)+'%', str(state_uninsured)+'%', str(us_uninsured)+'%'],
-        [ "Percentage of Population with Disability",str(county_disability)+'%', str(state_disability)+'%', str(us_disability)+'%',],
-        [ "Percentage of Population Aged 65 and older",str(county_age65)+'%', str(state_age65)+'%', str(us_age65)+'%'],
-        [  "Percentage of Population Aged 17 and younger",str(county_age17)+'%', str(state_age17)+'%', str(us_age17)+'%']]
+        cell_text = { 'countyName': county.iloc[0]["COUNTY"],
+                    'State': county.iloc[0]["STATE"].lower().title(),
+                'population': {'county': int(county_population), 'state':int(state_population),'us':int(us_population)},
+               'belowPovertyPopulation': {'county': county_poverty, 'state':state_poverty,'us':us_poverty},
+               'unemployedPopulation': {'county': county_unemp, 'state':state_unemp,'us':us_unemp},
+               'uninsuredPopulation': {'county': county_uninsured, 'state':state_uninsured,'us':us_uninsured},
+               'disabilityPopulation':{'county': county_disability, 'state':state_disability,'us':us_disability},
+               '65OlderPopulation': {'county': county_age65, 'state':state_age65,'us':us_age65,},
+               '17youngerPopulation': {'county': county_age17, 'state':state_age17,'us':us_age17}
+        }
+
+    
 
         # creating json file 
          # creating json file 
         json_string= json.dumps(cell_text, indent=4)
         with open("demographics/county."+fipscode+".json", 'w') as outfile:
             outfile.write(json_string)
+
+def waterfall_charts():
+    for index, row in NETCCN.iterrows():
+        fipscode = str(row["site.fips"])
+        provider_id= str(row['provider.num'])
+        if (provider_id=='0'):
+            continue
+            
+        if ('NaT' in str(row["StatusLogStatusActive_Complete"])):
+            waterfall_start= row["StatusLogStatusReceived_Start"]-dt.timedelta(weeks = 2)
+            waterfall_end= row["actualStartDate"]+dt.timedelta(days = 9)
+            request_date=row["StatusLogStatusReceived_Start"]
+            start_date=row["actualStartDate"] 
+        else: 
+            waterfall_start= row["StatusLogStatusReceived_Start"]-dt.timedelta(weeks = 2)
+            waterfall_end= row["StatusLogStatusActive_Complete"]+dt.timedelta(days = 9)
+            request_date=row["StatusLogStatusReceived_Start"]
+            start_date=row["actualStartDate"] 
+            end_date=row["StatusLogStatusActive_Complete"]
+        
+        ## Creating dataframe for specific hospital
+    
+        Specific_Hospital= utilizations[utilizations['hospital_pk']==provider_id]
+        if (Specific_Hospital.shape[0]==0):
+            continue
+        Specific_Hospital=Specific_Hospital[(Specific_Hospital['collection_week'] >= waterfall_start) & 
+                                        (Specific_Hospital['collection_week'] <= waterfall_end)]
+        Specific_Hospital_filtered= Specific_Hospital[["collection_week"]]
+        Specific_Hospital_filtered["inpatient_beds_used_covid_7_day_avg"]= Specific_Hospital["inpatient_beds_used_covid_7_day_sum"]/7
+        Specific_Hospital_filtered["staffed_icu_adult_patients_confirmed_covid_7_day_avg"]= Specific_Hospital["staffed_icu_adult_patients_confirmed_covid_7_day_sum"]/7
+        Specific_Hospital_filtered["total_covid_patients"]= Specific_Hospital_filtered["inpatient_beds_used_covid_7_day_avg"]+Specific_Hospital_filtered["staffed_icu_adult_patients_confirmed_covid_7_day_avg"]
+        
+        Specific_Hospital_filtered= Specific_Hospital_filtered.set_index("collection_week")
+        deltas = [Specific_Hospital_filtered['total_covid_patients'][i] if 
+            i==0 else Specific_Hospital_filtered['total_covid_patients'][i]-Specific_Hospital_filtered['total_covid_patients'][i-1] 
+            for i in range(len(Specific_Hospital_filtered))]
+        Specific_Hospital_filtered['delta'] = np.round(deltas,1)
+        Specific_Hospital_filtered=Specific_Hospital_filtered.reset_index()
+        Specific_Hospital_filtered["measure"]= "relative"
+        Specific_Hospital_filtered.iloc[-1,Specific_Hospital_filtered.columns.get_loc('measure')] = "total"
+        Specific_Hospital_filtered.iloc[-1,Specific_Hospital_filtered.columns.get_loc('delta')] = np.round(Specific_Hospital_filtered.iloc[-1,Specific_Hospital_filtered.columns.get_loc('total_covid_patients')],1)
+        
+        
+        fig = go.Figure(go.Waterfall(
+        orientation = "v",
+        measure = Specific_Hospital_filtered.measure,
+        x = Specific_Hospital_filtered.collection_week,
+        textposition = "inside",
+        text = Specific_Hospital_filtered.delta,
+        y = Specific_Hospital_filtered.delta,
+        connector = {"line":{"color":"rgb(63, 63, 63)"}},
+        ))
+        
+        #fig.update_xaxes(range=[-1, max(Specific_Hospital_filtered["total_covid_patients"])*1.05])
+        if ('NaT' in str(row["StatusLogStatusActive_Complete"])):
+            fig.add_trace(go.Scatter(
+            x=[request_date,start_date],
+            y=[-0.5, -0.5],
+            text=["<b>Request</b>",
+            "<b>Deployment <br> Start</b>"],
+            mode="text",
+            textposition="middle center",
+            textfont=dict(
+            size=12,
+            color="black"
+            )))
+        else: 
+            fig.add_trace(go.Scatter(
+            x=[request_date,start_date,end_date],
+            y=[-0.5, -0.5,-0.5],
+            text=["<b>Request</b>",
+            "<b>Deployment <br> Start</b>","<b>Deployment <br> End</b>" ],
+            mode="text",
+            textposition="middle center",
+            textfont=dict(
+            size=12,
+            color="black"
+            )))
+
+    
+
+        # Add shapes
+        
+    
+        fig.update_shapes(dict(xref='x', yref='y'))
+        fig.update_layout(
+            title = 'Number of COVID-19 patients in '+ Specific_Hospital.iloc[0]["hospital_name"].lower().title()+'<br> (weekly average) (inpatient beds + Staffed ICU beds)',
+            title_x=0.5,
+            yaxis_range=[-2, max(Specific_Hospital_filtered["total_covid_patients"])*1.05],
+            xaxis_title='Date',
+            yaxis_title="'Average number of COVID-19 patients'",
+            showlegend = False
+        )
+        
+        plotly.io.write_json(fig, "waterfall/county."+fipscode+"hospital."+provider_id+".plotly.json", pretty=True)
+
 covid_transmission_charts()
 covid_cases_charts()
 demographics_table()
+waterfall_charts()
 print("all done")
